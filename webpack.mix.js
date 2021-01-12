9a6e0c8773314f0b9bfa169428a3e57257367c42
Cristian Aguilar <cristian.aguilar02@gmail.com>
2021-01-11 19:27:56 -0600

    configuracion de plantilla del front-end

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,25 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix
+    .styles([
+        'resources/vendor/fontawesome-free-5.15.1-web/css/all.min.css',
+        'resources/css/adminlte.css',
+    ], 'public/css/app.css')
+
+    .js('resources/js/app.js', 'public/js')
+
+    .scripts([
+        'resources/vendor/jquery/jquery.min.js',
+        'resources/vendor/bootstrap/js/bootstrap.bundle.min.js'
+    ], 'public/js/vendor.js')
+
+
+    //.sass('resources/sass/app.scss', 'public/css')
+
+    //requiere dos parametros un archivo origen y un archivo destino
+    .copy('resources/vendor/fontawesome-free-5.15.1-web/webfonts', 'public/webfonts')
+
+    //limpiar o actualizar la cache del navegador en base a lo que se tenga en el servidor
+    .version() //los cambios se reflejan en el archivo public/mix-manifest.json
+    ;
